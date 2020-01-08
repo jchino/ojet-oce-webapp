1b20fdd8a34474e9a43913ad4e146027a9dbfbea
jchino <junko.chino@oracle.com>
2020-01-08 20:22:12 +0900

    impl upload to oce

--- src/js/main.js
+++ src/js/main.js
@@ -42,41 +42,139 @@ require([
   'knockout',
   'jquery',
   'ojs/ojbootstrap',
+  'ojs/ojarraydataprovider',
+  'ojs/ojLogger',
   'ojs/ojknockout',
+  'ojs/ojmessages',
   'ojs/ojformlayout',
   'ojs/ojinputtext',
   'ojs/ojbutton',
+  'ojs/ojselectsingle',
+  'ojs/ojfilepicker',
 ],
-function (ko, $, Bootstrap) {
+function (ko, $, Bootstrap, ArrayDataProvider, Logger) {
 
   const ViewModel = function () {
     this.authorized = ko.observable(false);
     this.baseUrl = ko.observable('https://hostname.domain/documents');
     this.username = ko.observable();
     this.password = ko.observable();
+    this.loginMsgs = ko.observableArray();
+    this.loginMsgsProvider = new ArrayDataProvider(this.loginMsgs);
+
+    this.folders = ko.observableArray();
+    this.folderId = ko.observable();
+    this.uploadFile = ko.observable();
+    this.uploadFileName = ko.pureComputed(function () {
+      if (this.uploadFile()) {
+        return this.uploadFile().name;
+      }
+      else {
+        return '未指定';
+      }
+    }.bind(this));
+    this.foldersProvider = new ArrayDataProvider(this.folders, { keyAttributes: 'value'});
+    this.uploadMsgs = ko.observableArray();
+    this.uploadMsgsProvider = new ArrayDataProvider(this.uploadMsgs);
 
     this.getUsersHomeContents = function () {
-      this.authorized(true);
+      this.loginMsgs.removeAll();
+      $.ajax({
+        url: this.baseUrl() + '/api/1.2/folders/items',
+        method: 'GET',
+        headers: {
+          'Authorization': 'Basic ' + btoa(this.username() + ':' + this.password())
+        }
+      }).then(
+        function (data) {
+          this.folders.removeAll();
+          for (let i = 0; i < data.items.length; i++) {
+            if (data.items[i].type !== 'folder') {
+              continue;
+            }
+            this.folders.push({
+              value: data.items[i].id,
+              label: data.items[i].name
+            });
+          }
+          this.authorized(true);
+        }.bind(this),
+        function (jqXHR) {
+          Logger.error(jqXHR.status, jqXHR.statusText);
+          let summary;
+          switch (jqXHR.status) {
+            case 401:
+              summary = 'ユーザー名またはパスワードが無効です';
+              break;
+            case 404:
+              summary = 'OCE の URL が無効です';
+              break;
+            default:
+              summary = 'OCE にアクセスできません';
+              break;
+          }
+          this.loginMsgs.push({
+            severity: 'error',
+            summary: summary
+          });
+        }.bind(this)
+      );
+    }.bind(this);
+
+    this.executeUpload = function () {
+      this.uploadMsgs.removeAll();
+      const data = new FormData();
+      data.append('jsonInputParameters', JSON.stringify({ parentID: this.folderId() }));
+      data.append('primaryFile', this.uploadFile());
+      $.ajax({
+        url: this.baseUrl() + '/api/1.2/files/data',
+        method: 'POST',
+        mimeType: "multipart/form-data",
+        contentType: false,
+        processData: false,
+        data: data,
+        headers: {
+          'Authorization': 'Basic ' + btoa(this.username() + ':' + this.password())
+        }
+      }).then(
+        function () {
+          this.uploadMsgs.push({
+            severity: 'info',
+            summary: 'アップロードが完了しました'
+          });
+        }.bind(this),
+        function (jqXHR) {
+          Logger.error(jqXHR.status, jqXHR.statusText);
+          this.uploadMsgs.push({
+            severity: 'error',
+            summary: 'アップロードできませんでした',
+            detail: jqXHR.status + ' - ' + jqXHR.statusText
+          });
+        }.bind(this)
+      );
+    }.bind(this);
+
+    this.fileSelctAction = function (event) {
+      this.uploadFile(event.detail.files[0]);
     }.bind(this);
 
     // eslint-disable-next-line no-unused-vars
     this.authorized.subscribe(function(newValue) {
-      $('#loginPanel').toggle();
-      $('#docsPanel').toggle();
+      document.getElementById('loginPanel').classList.toggle('oj-helper-hidden');
+      document.getElementById('docsPanel').classList.toggle('oj-helper-hidden');
     }.bind(this));
   };
   
-  Bootstrap.whenDocumentReady().then(
-    function () {
-      const init = function () {
-        ko.applyBindings(new ViewModel(), document.getElementById('pageBody'));
-      };
+  Bootstrap.whenDocumentReady().then(function () {
+    const init = function () {
+      ko.applyBindings(new ViewModel(), document.getElementById('pageBody'));
+    };
 
-      if (document.body.classList.contains('oj-hybrid')) {
-        document.addEventListener('deviceready', init);
-      }
-      else {
-        init();
-      }
-    });
+    if (document.body.classList.contains('oj-hybrid')) {
+      document.addEventListener('deviceready', init);
+    }
+    else {
+      init();
+    }
+  });
 });
